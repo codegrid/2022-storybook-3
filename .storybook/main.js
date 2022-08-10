1003f552cf6c74253bfc5f811470d7ce357fc6a1
tomof <fffjt007@gmail.com>
2022-08-10 18:09:10 +0900

    Storybookの不具合の対応

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
+  typescript: {
+    // 不具合の対応：https://github.com/storybookjs/storybook/issues/15336
+    reactDocgen: false
+  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
