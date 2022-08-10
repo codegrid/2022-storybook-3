322188b5a27ba49bf5798f25aa1506e9b66aebc6
tomof <fffjt007@gmail.com>
2022-08-10 18:09:24 +0900

    サイドバー、ツールバー、Canvasの設定例を追加

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,4 +6,47 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  // コンポーネントを中央に配置
+  // layout: 'centered',
+  backgrounds: {
+    // ツールバーから選択できる背景色の指定
+    default: 'white',
+    values: [
+      {
+        name: 'white',
+        value: '#ffffff',
+      },
+      {
+        name: 'black',
+        value: '#222222',
+      },
+      {
+        name: 'grey',
+        value: '#cccccc',
+      },
+    ],
+    // ツールバーから表示するグリッド表示の指定
+    grid: {
+      cellSize: 10, // 1マスの高さ・幅のサイズ（px単位）
+      opacity: 0.5, // グリッド線の透明度
+      cellAmount: 10, // 指定されたマスごとにグリッド線を強調する
+      offsetX: 16, // cellAmountで強調する最初のグリッド線のX軸の位置
+      offsetY: 16, // cellAmountで強調する最初のグリッド線のY軸の位置
+    },
+  },
+  options: {
+    storySort: {
+      // サイドバーの並び順を制御
+      order: ['UI', 'Example', ['Introduction', 'Page', 'Header', 'Button']],
+    },
+  },
 }
+
+// デコレーターの指定
+// export const decorators = [
+//   (Story) => (
+//     <div style={{ border: '1px dashed #aaaaaa', padding: '20px', borderRadius:' 20px' }}>
+//       <Story />
+//     </div>
+//   ),
+// ]
--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -4,7 +4,29 @@ import { Button } from './Button'
 // コンポーネントの概要
 export default {
   title: 'UI/Button',
+  // 'title: 'Design System/UI/Button' // 見出しがDesign Systemになり、階層が1つ深くなる
   component: Button,
+  parameters: {
+  //   // コンポーネントを中央に配置（コンポーネント限定の指定）
+  //   layout: 'centered',
+  //   // このコンポーネント限定の背景色の設定を適用（コンポーネント限定の指定）
+  //   backgrounds: {
+  //     values: [
+  //       { name: 'red', value: '#f00' },
+  //       { name: 'green', value: '#0f0' },
+  //       { name: 'blue', value: '#00f' },
+  //     ],
+  //   },
+  },
+
+  // デコレーターを指定（コンポーネント限定の指定）
+  // decorators: [
+  //   (Story) => (
+  //     <div style={{ border: '1px dashed #aaaaaa', padding: '20px', borderRadius:' 20px' }}>
+  //       <Story />
+  //     </div>
+  //   ),
+  // ],
 }
 
 // Buttonストーリーのテンプレートを作成
@@ -12,12 +34,36 @@ const Template = (args) => <Button {...args}>ボタン</Button>
 
 // 6つのストーリー
 export const Default = Template.bind({})
+// レイアウトを指定（Defaultストーリー限定）
+Default.parameters = {
+  layout: 'centered',
+};
+// デコレーターを指定（Defaultストーリー限定）
+Default.decorators = [
+  (Story) => (
+    <div style={{ border: '1px dashed #aaaaaa', padding: '20px', borderRadius:' 20px' }}>
+      <Story />
+    </div>
+  ),
+];
 
 export const FullWidth = Template.bind({})
 FullWidth.args = { ...FullWidth.args, fullWidth: true }
+// ストーリー名を変更
+FullWidth.storyName = 'FullWidth（モバイル向け）'
 
-export const Large = Template.bind({})
-Large.args = { ...Large.args, size: 'large' }
+// 背景色の設定を適用（Largeストーリー限定）
+// export const Large = Template.bind({})
+// Large.args = { ...Large.args, size: 'large' }
+// Large.parameters = {
+//   backgrounds: {
+//     values: [
+//       { name: 'red', value: '#f00' },
+//       { name: 'green', value: '#0f0' },
+//       { name: 'blue', value: '#00f' },
+//     ],
+//   },
+// };
 
 export const Attention = Template.bind({})
 Attention.args = { ...Attention.args, variant: 'attention' }
